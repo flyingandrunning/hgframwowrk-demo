331c8920183150e7cc25f26dc3b29470c24b6cf1
huanggang <huanggang@1391.com>
2017-12-05 14:46:50 +0800

    各种应用demo，以及部分的debug注解等

---
+++ src/main/java/com/hgframework/demo/aop/DemoAopMain.java
@@ -0,0 +1,16 @@
+package com.hgframework.demo.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/12/5 0005.
+ */
+
+@SpringBootApplication
+public class DemoAopMain {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoAopMain.class, args);
+    }
+}
+
